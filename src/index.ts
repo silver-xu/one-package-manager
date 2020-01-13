4467b8710b2772cc37efaed6bb75c117265a8a3e
Silver Xu <me@silverxu.com>
2020-01-13 22:09:22 +1100

    feat(validate): validate package manager

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
-export const greet = (name: string): string => `Hi ${name}!`;
+import slash from 'slash';
 
-console.log(greet('World'));
+const packageJson = require(process.cwd() + '/package.json');
+
+export const usingNPM = (): boolean => {
+  const nex = process.env.npm_execpath;
+  return Boolean(nex && /node_modules\/npm/.test(slash(nex)));
+};
+
+export const validatePackageManager = (settings: any) => {
+  let enforcePackageManager = settings?.onePackageManager?.enforcePackageManager;
+
+  if (!enforcePackageManager || !['yarn', 'npm'].find(value => value === enforcePackageManager.toLowerCase())) {
+    enforcePackageManager = 'yarn';
+  }
+
+  if (usingNPM()) {
+    if (enforcePackageManager === 'yarn') {
+      throw new Error('OnePackageManager: Please use Yarn instead');
+    }
+  } else {
+    if (enforcePackageManager === 'npm') {
+      throw new Error('OnePackageManager: Please use NPM instead');
+    }
+  }
+};
+
+validatePackageManager(packageJson);
