f518b728e6c443b9e21d5bfd19ffe06759a99b61
Silver Xu <me@silverxu.com>
2020-01-14 22:28:49 +1100

    fix(build): fixing Build

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,13 @@ module.exports = {
   roots: ['src'],
   transform: { '\\.ts$': ['ts-jest'] },
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{ts,js}'],
+  collectCoverageFrom: ['src/index.{ts,js}'],
   coverageThreshold: {
     global: {
-      branches: 60,
-      functions: 60,
-      lines: 60,
-      statements: 60,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
   },
 };
